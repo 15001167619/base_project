688e0424b0338a11f4cc264d52b77a2eb342f2e9
wuhaisheng <123456>
2019-05-09 08:27:50 +0800

    更新sql

--- admin/src/main/java/com/etycx/Application.java
+++ admin/src/main/java/com/etycx/Application.java
@@ -13,6 +13,6 @@ public class Application
 {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
-        log.info("AnswerApplication is start up success");
+        log.info("Application is start up success");
     }
 }
